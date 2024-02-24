89090bc32cfe6fef36c1cae0a23e2e3ef102a806
Yong Sheng Tan <yongsheng.tan@shareinvestor.com>
2024-02-24 14:40:29 +0800

    update

--- 0. websocket/README.md
+++
@@ -1,3 +0,0 @@
-# Scraping real-time data on the web
-
-Reference: https://pub.towardsai.net/why-you-should-use-websocket-to-retrieve-live-crypto-data-27d3b29f210c
--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -12,8 +16,9 @@ author = 'Tan Yong Sheng'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = ["myst-parser"]
+extensions = ["myst_parser", 
+              "nbsphinx", 
+              "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
