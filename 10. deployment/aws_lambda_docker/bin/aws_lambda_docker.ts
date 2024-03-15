a3d23759375128f94af884d93579970af144f143
Yong Sheng Tan <64836390+tan-yong-sheng@users.noreply.github.com>
2024-03-15 22:31:27 +0800

    update

--- 10. deployment/aws_lambda_docker/bin/aws_lambda_docker.ts
+++ 10. deployment/aws_lambda_docker/bin/aws_lambda_docker.ts
@@ -1,21 +1,31 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AwsLambdaDockerStack } from '../lib/aws_lambda_docker-stack';
+import { Construct } from 'constructs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
-const app = new cdk.App();
-new AwsLambdaDockerStack(app, 'AwsLambdaDockerStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+export class DockerLambdaAwsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+    const dockerFunc = new lambda.DockerImageFunction(this, "DockerFunc", {
+      code: lambda.DockerImageCode.fromImageAsset("./image"),
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(10),
+      architecture: lambda.Architecture.ARM_64,
+    });
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+    const functionUrl = dockerFunc.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+        allowedOrigins: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "FunctionUrlValue", {
+      value: functionUrl.url,
+    });
+  }
+}
