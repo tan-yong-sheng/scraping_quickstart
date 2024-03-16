8ec53df51d7cf31f3d538d39c8182fcbce6485ed
Tan Yong Sheng <64836390+tan-yong-sheng@users.noreply.github.com>
2024-03-16 10:36:25 +0000

    update

--- 10_deployment/aws_lambda_docker/image/src/main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def handler(event, context):
-    arr = np.random.randint(0,10,size=(3,3))
-    return {
-        "statusCode": 200, 
-        "body": "Hello World", 
-        "array": arr.tolist()
-    }
