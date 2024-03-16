92d10086cf5004442533b59ec4987ebfb8bbd33e
Tan Yong Sheng <64836390+tan-yong-sheng@users.noreply.github.com>
2024-03-16 09:51:09 +0000

    update

---
+++ 10_deployment/aws_lambda_docker2/test/aws_lambda_docker2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsLambdaDocker2 from '../lib/aws_lambda_docker2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_lambda_docker2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsLambdaDocker2.AwsLambdaDocker2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
