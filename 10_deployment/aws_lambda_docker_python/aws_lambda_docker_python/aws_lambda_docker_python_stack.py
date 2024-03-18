ae345fc6ed07c491ca8075bd0a9f0b387ce5965a
Tan Yong Sheng <64836390+tan-yong-sheng@users.noreply.github.com>
2024-03-18 12:02:24 +0000

    update

--- 10_deployment/aws_lambda_docker_python/aws_lambda_docker_python/aws_lambda_docker_python_stack.py
+++ 10_deployment/aws_lambda_docker_python/aws_lambda_docker_python/aws_lambda_docker_python_stack.py
@@ -1,31 +1,35 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    aws_sqs,
-    aws_lambda
+    # aws_sqs,
+    aws_lambda,
+    aws_apigateway,
+    Duration
 )
 from constructs import Construct
 
 class AwsLambdaDockerPythonStack(Stack):
-
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        #queue = aws_sqs.Queue(
+        #    self, "AwsLambdaDockerPythonQueue",
+        #    visibility_timeout=Duration.seconds(300),
+        #)
 
-        # example resource
-        queue = aws_sqs.Queue(
-            self, "AwsLambdaDockerPythonQueue",
-            visibility_timeout=Duration.seconds(300),
+        lambda_function = aws_lambda.DockerImageFunction(
+            self, "DockerFunc",
+                memory_size=512,
+                code = aws_lambda.DockerImageCode.from_image_asset("./image"),
+                timeout = Duration.seconds(10),
+                architecture = aws_lambda.Architecture.X86_64
         )
 
-        lambda_function = aws_lambda.AwsLambdaDockerPythonStack(
-            self, "AwsLambdaDockerPythonStack",
-            memory_size = 512,
-            timeout = Duration.seconds(10),
-            code = aws_lambda.Code.from_asset("./image"),
-            runtime = aws_lambda.Runtime.PYTHON_3_10,
-            architecture = aws_lambda.Architecture.X86_64,
-
+        aws_apigateway.LambdaRestApi(
+            self, "Endpoint",
+            handler=lambda_function,
         )
-    
+
+
+
+        
